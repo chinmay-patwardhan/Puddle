34b5714663a68f7f549cc751ea3a428509af6b9c
Chinmay Patwardhan <patwardhan.chinmay@gmail.com>
2023-08-01 13:17:25 +0530

    can not add, edit and delete items

--- puddle/settings.py
+++ puddle/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-    'item'
+    'item',
+    'dashboard',
 ]
 
 MIDDLEWARE = [
--- puddle/urls.py
+++ puddle/urls.py
@@ -22,5 +22,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
-    path('items/',include('item.urls')),
+    path('items/', include('item.urls')),
+    path('dashboard/', include('dashboard.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
